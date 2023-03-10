2315a9dd621e0cecf0f198dae6edf944652b77b7
cerensemiz <SABAH@DESKTOP-E30TBPJ>
2023-03-10 11:36:08 +0300

    .

--- CafeOtomasyonu/Form1.Designer.cs
+++ CafeOtomasyonu/Form1.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(665, 475);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
