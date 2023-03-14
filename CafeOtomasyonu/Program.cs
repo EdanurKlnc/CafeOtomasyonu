17ef411899c718caa042e8b6926465ceabd3bf9e
cerensemiz <SABAH@DESKTOP-E30TBPJ>
2023-03-14 14:35:05 +0300

    .

--- CafeOtomasyonu/Program.cs
+++ CafeOtomasyonu/Program.cs
@@ -13,7 +13,7 @@ namespace CafeOtomasyonu
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AnaMenu());
+            Application.Run(new Form1());
         }
     }
 }
