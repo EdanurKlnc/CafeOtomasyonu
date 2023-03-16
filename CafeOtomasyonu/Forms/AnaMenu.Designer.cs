ed86bec087f1c34bc4b1cd91f228ab844a084b85
cerensemiz <SABAH@DESKTOP-E30TBPJ>
2023-03-16 13:17:39 +0300

    Buttons changed.

--- CafeOtomasyonu/Forms/AnaMenu.Designer.cs
+++ CafeOtomasyonu/Forms/AnaMenu.Designer.cs
@@ -53,14 +53,14 @@
             tblAna.Name = "tblAna";
             tblAna.RowCount = 1;
             tblAna.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tblAna.Size = new Size(793, 499);
+            tblAna.Size = new Size(743, 486);
             tblAna.TabIndex = 0;
             // 
             // flpKatBilgisi
             // 
             flpKatBilgisi.Location = new Point(3, 3);
             flpKatBilgisi.Name = "flpKatBilgisi";
-            flpKatBilgisi.Size = new Size(180, 493);
+            flpKatBilgisi.Size = new Size(147, 453);
             flpKatBilgisi.TabIndex = 0;
             // 
             // tableLayoutPanel1
@@ -71,13 +71,13 @@
             tableLayoutPanel1.Controls.Add(flpKategoriler, 0, 1);
             tableLayoutPanel1.Controls.Add(flpUrunler, 0, 2);
             tableLayoutPanel1.Dock = DockStyle.Fill;
-            tableLayoutPanel1.Location = new Point(192, 3);
+            tableLayoutPanel1.Location = new Point(180, 3);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 3;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
-            tableLayoutPanel1.Size = new Size(438, 493);
+            tableLayoutPanel1.Size = new Size(410, 480);
             tableLayoutPanel1.TabIndex = 1;
             // 
             // flpMasalar
@@ -86,23 +86,23 @@
             flpMasalar.Dock = DockStyle.Fill;
             flpMasalar.Location = new Point(3, 3);
             flpMasalar.Name = "flpMasalar";
-            flpMasalar.Size = new Size(432, 191);
+            flpMasalar.Size = new Size(404, 186);
             flpMasalar.TabIndex = 0;
             // 
             // flpKategoriler
             // 
             flpKategoriler.Dock = DockStyle.Fill;
-            flpKategoriler.Location = new Point(3, 200);
+            flpKategoriler.Location = new Point(3, 195);
             flpKategoriler.Name = "flpKategoriler";
-            flpKategoriler.Size = new Size(432, 92);
+            flpKategoriler.Size = new Size(404, 90);
             flpKategoriler.TabIndex = 1;
             // 
             // flpUrunler
             // 
             flpUrunler.Dock = DockStyle.Fill;
-            flpUrunler.Location = new Point(3, 298);
+            flpUrunler.Location = new Point(3, 291);
             flpUrunler.Name = "flpUrunler";
-            flpUrunler.Size = new Size(432, 192);
+            flpUrunler.Size = new Size(404, 186);
             flpUrunler.TabIndex = 2;
             // 
             // tableLayoutPanel2
@@ -110,12 +110,12 @@
             tableLayoutPanel2.ColumnCount = 1;
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
             tableLayoutPanel2.Dock = DockStyle.Fill;
-            tableLayoutPanel2.Location = new Point(636, 3);
+            tableLayoutPanel2.Location = new Point(596, 3);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 2;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 60F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
-            tableLayoutPanel2.Size = new Size(154, 493);
+            tableLayoutPanel2.Size = new Size(144, 480);
             tableLayoutPanel2.TabIndex = 2;
             // 
             // AnaMenu
@@ -123,7 +123,7 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            ClientSize = new Size(793, 499);
+            ClientSize = new Size(743, 486);
             Controls.Add(tblAna);
             Name = "AnaMenu";
             Text = "AnaMenu";
